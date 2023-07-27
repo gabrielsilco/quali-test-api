fb63a1a4845ea98509e56af66f84a28dd415f1a3
gabrielsilco <gabriel.silco@gmail.com>
2023-07-27 14:40:51 -0300

    Resolvendo questões de CORS

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 const restify = require('restify');
-const routes = require('./routes')
+const cors = require('cors');
+
+const routes = require('./routes');
 
 const server = restify.createServer();
 
+const corsOptions = {
+  origin: 'http://localhost:8080',
+  methods: 'GET, POST, PUT, DELETE, OPTIONS',
+  allowedHeaders: 'Content-Type, Authorization',
+};
+
 server.use(restify.plugins.bodyParser());
+server.use(cors(corsOptions))
+
+server.opts('*', cors());
+server.del('*', cors());
 
 routes(server);
 
